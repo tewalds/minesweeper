db0c07d2b1bce6d563eb2c570253b71a0a8b88ac
Timo Ewalds <timo@ewalds.ca>
2025-02-21 13:43:48 +0000

    Update the random_test to use the catch2 seed, and add a benchmark.

--- src/random_test.cc
+++ src/random_test.cc
@@ -8,11 +8,11 @@ TEMPLATE_TEST_CASE("Random", "[random]", Splitmix64, Xoshiro256pp) {
 
   SECTION("seed") {
     REQUIRE(TestType(42)() == TestType(42)());
-    REQUIRE(TestType()() != TestType(42)());
+    REQUIRE(TestType(1)() != TestType(42)());
   }
 
   SECTION("many") {
-    TestType rng;
+    TestType rng(Catch::getSeed());
     std::vector<uint64_t> values;
     for (int i = 0; i < 100; ++i) {
       values.push_back(rng());
@@ -24,6 +24,17 @@ TEMPLATE_TEST_CASE("Random", "[random]", Splitmix64, Xoshiro256pp) {
   }
 
   SECTION("uniform") {
-    REQUIRE(absl::Uniform(TestType(), 0.0, 1.0) < 1.0);
+    TestType rng(Catch::getSeed());
+    for (int i = 0; i < 100; ++i) {
+      double v = absl::Uniform(rng, 0.0, 1.0);
+      REQUIRE((v >= 0.0 && v < 1.0));
+    }
+  }
+
+  SECTION("benchmark") {
+    TestType rng(Catch::getSeed());
+    BENCHMARK("generate") {
+      return rng();
+    };
   }
 }
